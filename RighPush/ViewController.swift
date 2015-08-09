b87afe7e9c3c651e238ff1c37a7f271762e8e22b
Sebastian <sebastian@bitops.io>
2015-08-09 11:55:33 -0700

    first attempt at right slide

--- RighPush/ViewController.swift
+++ RighPush/ViewController.swift
@@ -2,8 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let x = UIView()
-    let y = UIView()
+    var x = UIView()
+    var y = UIView()
     
     let animationDuration = 3.5;
     let delayTime = 1.5;
@@ -20,16 +20,50 @@ class ViewController: UIViewController {
         view.addSubview(y)
     }
     
+    @IBAction func push() {
+        let x = self.view.frame.origin.x
+        let y = self.view.frame.origin.y
+        let animationTime = 3.0;
+        
+        UIView.animateWithDuration(animationTime,
+            animations: {
+                self.view.frame = CGRectMake(x - 250, y, self.view.frame.width, self.view.frame.height)
+            },
+            completion: { (var b) in
+                UIView.animateWithDuration(animationTime, animations: {
+                    self.view.frame = CGRectMake(x, y, self.view.frame.width, self.view.frame.height)
+                })
+        })
+        
+    }
+    
+    @IBAction func reset() {
+        x.removeFromSuperview()
+        y.removeFromSuperview()
+       
+        x = UIView()
+        x.frame = CGRectMake(50, 50, 100, 100)
+        x.backgroundColor = UIColor.redColor()
+        
+        y = UIView()
+        y.frame = CGRectMake(150, 150, 100, 100)
+        y.backgroundColor = UIColor.blueColor()
+        
+        view.addSubview(x)
+        view.addSubview(y)
+    }
+    
+    
     @IBAction func action() {
         UIView.animateWithDuration(animationDuration,
             animations: {
                 self.x.backgroundColor = UIColor.greenColor()
                 self.x.frame = CGRectMake(50, 150, 100, 100)
-                //self.x.transform = CGAffineTransformMakeRotation(45.0)
+                self.x.transform = CGAffineTransformMakeRotation(45.0)
                 
                 self.y.backgroundColor = UIColor.greenColor()
                 self.y.frame = CGRectMake(150, 50, 100, 100)
-                //self.y.transform = CGAffineTransformMakeRotation(45.0)
+                self.y.transform = CGAffineTransformMakeRotation(-45.0)
             },
             completion: { (var b) in
                 UIView.animateWithDuration(self.animationDuration, delay: self.delayTime / 2, options: .CurveEaseOut, animations: {
